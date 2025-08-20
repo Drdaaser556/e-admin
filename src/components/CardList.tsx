d9018013316b3aaf9e544cdb43c7166dbb8dcdfa
Deen Mohammad Dewan <deenmohammaddewan@gmail.com>
2025-08-20 13:58:17 +0600

    Card list updated

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -93,8 +93,34 @@ const CardList = ({ title }: { title: string }) => {
   const list =
     title === "Popular Products" ? popularProducts : latestTransactions;
   return (
-    <div className="">
-      <h1 className="text-lg font-medium mb-6">{title}</h1>
+    <>
+    <h1 className="text-lg font-medium mb-6">{title}</h1>
+    <div className="flex gap-4 p-4">
+      
+      <div className="flex flex-col gap-2">
+        {list.map((item) => (
+          <Card
+            key={item.id}
+            className="flex-row items-center justify-between gap-4 p-4"
+          >
+            <div className="w-12 h-12 rounded-sm relative overflow-hidden">
+              <Image
+                src={item.image}
+                alt={item.title}
+                fill
+                className="object-cover"
+              />
+            </div>
+            <CardContent className="flex-1 p-0">
+              <CardTitle className="text-sm font-medium">
+                {item.title}
+              </CardTitle>
+              <Badge variant="secondary">{item.badge}</Badge>
+            </CardContent>
+            <CardFooter className="p-0">{item.count / 1000}K</CardFooter>
+          </Card>
+        ))}
+      </div>
       <div className="flex flex-col gap-2">
         {list.map((item) => (
           <Card
@@ -120,6 +146,7 @@ const CardList = ({ title }: { title: string }) => {
         ))}
       </div>
     </div>
+    </>
   );
 };
 
